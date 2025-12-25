2e772e5017e7ae90052c7add414e0bef5814bc01
kiru-she12 <kirushika1211@gmail.com>
2025-12-25 18:25:45 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-function App(){
+import { useState } from "react";
+
+function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task !== "") {
+      setTasks([...tasks, task]);
+      setTask("");
+    }
+  };
+
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Student Task Manager</h1>
+
+      <input
+        type="text"
+        placeholder="Enter a task"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+
+      <button onClick={addTask}>Add Task</button>
+
+      <ul>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
+      </ul>
     </div>
-  ); 
+  );
 }
 
 export default App;
+
